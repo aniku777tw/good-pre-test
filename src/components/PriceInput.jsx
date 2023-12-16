a845b756edc84422943b7ab4db006c7aa92db242
Hanz <e102777tw@gmail.com>
2023-12-17 05:13:04 +0800

    feat: handle cursor position when editing in PriceInput

--- src/components/PriceInput.jsx
+++ src/components/PriceInput.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Typography, Space, Flex, Input } from "antd";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import Utils from "../utils/utils";
 import AlertBox from "./AlertBox";
@@ -12,24 +12,38 @@ const StyledInputNumber = styled(Input)`
 
 function PriceInput({ onChange }) {
   const [status, setStatus] = useState("");
-  const [stringNumber,setStringNumber] = useState(0);
+  const [stringNumber, setStringNumber] = useState(0);
+  const inputRef = useRef(null);
+  const focused = useRef();
 
   const handleInputChange = (e) => {
-    
-    const noCommaStrNumber =e.target.value.replaceAll(/,/g, '')
+    const noCommaStrNumber = e.target.value.replaceAll(/,/g, "");
 
     const checkInputCharRegex = /^\d+(\.\d*)?$/;
-    if(!checkInputCharRegex.test(noCommaStrNumber) && noCommaStrNumber){
-        return
+    if (!checkInputCharRegex.test(noCommaStrNumber) && noCommaStrNumber) {
+      return;
     }
 
     onChange({ price: e.target.value ? parseFloat(noCommaStrNumber) : null });
-    setStringNumber(Utils.addComma(noCommaStrNumber))
+    setStringNumber(Utils.addComma(noCommaStrNumber));
 
     noCommaStrNumber ? setStatus("") : setStatus("error");
+
+    focused.current = [e.target.value, e.target.selectionStart];
   };
 
-  
+  useEffect(() => {
+    if (!focused.current) {
+      return;
+    }
+    const [prevText, cursor] = focused.current;
+    const prevCommaCount = prevText.split(",").length - 1;
+    const currentCommaCount = stringNumber.split(",").length - 1;
+    const offset = currentCommaCount - prevCommaCount;
+    const curr = cursor + offset;
+    const pos = curr < 0 ? 0 : curr;
+    inputRef.current.setSelectionRange(pos, pos);
+  }, [stringNumber]);
 
   return (
     <Space direction="vertical">
@@ -40,10 +54,10 @@ function PriceInput({ onChange }) {
             addonBefore={
               <Typography.Text type="secondary">TWD</Typography.Text>
             }
+            ref={inputRef}
             value={stringNumber}
-            onChange={(e) => handleInputChange(e)}
+            onInput={(e) => handleInputChange(e)}
             placeholder="請輸入費用"
-            controls={false}
             status={status}
           />
         </Space.Compact>
